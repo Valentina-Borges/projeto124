46f123a0095275edabd857606f06f41046334708
Valentina-Borges <108351920+Valentina-Borges@users.noreply.github.com>
2023-04-11 16:52:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+diffeerence = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+    function setup() {
+        video = createCapture(VIDEO);
+        video.size(550, 500);
+
+        canvas = createCanvas(550, 550)  
+        canvas.position(560,150);
+
+        poseNet = ml5.poseNet(video, modelLoaded);
+        poseNet.on('pose', gotPose);
+    }
+
+    function modelLoaded() {
+        
+        console.log('PoseNet Is Initialized!');
+        }
